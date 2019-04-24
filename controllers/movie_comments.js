849567073f61af7556927d1893c56a4148cd2ca0
Eli <elizabethgbarrera@gmail.com>
2019-04-24 18:37:10 -0300

    commentsTODO

--- controllers/movie_comments.js
+++ controllers/movie_comments.js
@@ -3,7 +3,7 @@ var router = express.Router()
 var MovieComments = require('../models/movie_comments')
 var Users = require('../models/users')
 
-// List comments by movie
+// List comments by movie, allow unsigned user
 router.get('/:imdb_id', function (req, res) {
   let userId = req.user ? req.user.id : '00000000-0000-0000-0000-00000000'
 
@@ -32,68 +32,76 @@ router.get('/:imdb_id', function (req, res) {
     })
 })
 
-// Create a New Movie Rating & Comment
+// Create a New Movie Rating & Comment, only signed user
 router.post('/', function (req, res) {
-  Users.findOne({ id: req.body.user_id }, (err, user) => {
-    if (user) {
-      var comment = MovieComments({
-        user: user, // TODO: middleware
-        imdb_id: req.body.imdb_id,
-        comment: req.body.comment,
-        stars: req.body.stars
-      })
+  if (req.user) {
+    Users.findOne({ id: req.user.id }, (err, user) => {
+      if (user) {
+        var comment = MovieComments({
+          user: user,
+          imdb_id: req.body.imdb_id,
+          comment: req.body.comment,
+          stars: req.body.stars
+        })
 
-      comment.save()
-        .then(newComment => {
-          res.json({ comment: newComment })
-        })
-        .catch(err => {
-          res.json({ message: 'Something went wrong', error: err.message })
-        })
-    } else {
-      res.json({ errors: 'user not found' })
-    }
-  })
+        comment.save()
+          .then(newComment => {
+            res.json({ comment: newComment })
+          })
+          .catch(err => {
+            res.json({ message: 'Something went wrong', error: err.message })
+          })
+      } else {
+        res.json({ errors: 'user not found' })
+      }
+    })
+  } else {
+    res.json({ errors: 'please sing in' })
+  }
 })
 
-// Update a rating & comment
+// Update a rating & comment, only signed user
 router.put('/:id', function (req, res) {
-  let userId = req.query.user_id // TODO: middleware
-
-  MovieComments.findOne({ id: req.params.id, user_id: userId }, (err, result) => {
-    if (result) {
-      MovieComments.updateOne({ id: req.params.id }, {
-        comment: req.body.comment,
-        stars: req.body.stars
-      }).then((err, comment) => {
-        res.json({ result: 'ok' })
-      })
-        .catch(err => {
-          res.json({ message: 'Something went wrong', error: err.message })
-        })
-    } else {
-      res.json({ errors: 'rating not found' })
-    }
-  })
-})
-
-// Delete a rating & comment
-router.delete('/:id', function (req, res) {
-  let userId = req.query.user_id // TODO: middleware
-
-  MovieComments.findOne({ id: req.params.id, user_id: userId }, (err, result) => {
-    if (result) {
-      MovieComments.deleteOne({ id: req.params.id })
-        .then((err, comment) => {
+  if (req.user) {
+    MovieComments.findOne({ id: req.params.id, user: req.user._id }, (err, result) => {
+      if (result) {
+        MovieComments.updateOne({ id: req.params.id }, {
+          comment: req.body.comment,
+          stars: req.body.stars
+        }).then((err, comment) => {
           res.json({ result: 'ok' })
         })
-        .catch(err => {
-          res.json({ message: 'Something went wrong', error: err })
-        })
-    } else {
-      res.json({ errors: 'rating not found' })
-    }
-  })
+          .catch(err => {
+            res.json({ message: 'Something went wrong', error: err.message })
+          })
+      } else {
+        res.json({ errors: 'rating not found' })
+      }
+    })
+  } else {
+    res.json({ errors: 'please sing in' })
+  }
+})
+
+// Delete a rating & comment, only signed user
+router.delete('/:id', function (req, res) {
+  if (req.user) {
+    MovieComments.findOne({ id: req.params.id, user: req.user._id }, (err, result) => {
+      if (result) {
+        MovieComments.deleteOne({ id: req.params.id })
+          .then((err, comment) => {
+            res.json({ result: 'ok' })
+          })
+          .catch(err => {
+            res.json({ message: 'Something went wrong', error: err })
+          })
+      } else {
+        res.json({ errors: 'rating not found' })
+      }
+    })
+  } else {
+    res.json({ errors: 'please sing in' })
+  }
 })
 
 module.exports = router
