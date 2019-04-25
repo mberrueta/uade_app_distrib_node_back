f4173b38e363229fd7c1153d7b5fc69cff1f64ba
matt <matiasberrueta@gmail.com>
2019-04-25 19:39:03 -0300

    Fix token

--- controllers/auth.js
+++ controllers/auth.js
@@ -4,16 +4,25 @@ var Auth = require('../helpers/hash')
 var Key = require('../key')
 var JWT = require('jsonwebtoken')
 var Users = require('../models/users')
+var _ = require('lodash');
+var merge = require('lodash.merge');
 
 // Signing
 router.post('/signin', function (req, res) {
   Users
     .findOne({ email: req.body.email.toLowerCase().trim() }, (err, result) => {
       if (result) {
+
         Auth.hash_compare(req.body.pass, result.digest, match => {
           if (match) {
-            let token = JWT.sign({ user_id: result.id, email: result.email, user_name: result.name }, Key.tokenKey)
-            res.status(200).json({ user_id: result.id, email: result.email, user_name: result.name, token })
+            const jwt_token = JWT.sign({
+                          user_id: result.id,
+                          email: result.email,
+                          user_name: result.name
+                        }, Key.tokenKey)
+
+            let json = Object.assign({}, result._doc, { token: jwt_token})
+            res.status(200).json(json)
           } else {
             res.status(403).json({ message: 'Invalid Password/Username' })
           }
