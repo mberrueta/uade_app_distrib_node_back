24e76fb8676cfc54ce40ed8d5b47ce934a00818f
Eli <elizabethgbarrera@gmail.com>
2019-04-25 20:18:05 -0300

    lintAuth

--- controllers/auth.js
+++ controllers/auth.js
@@ -4,24 +4,21 @@ var Auth = require('../helpers/hash')
 var Key = require('../key')
 var JWT = require('jsonwebtoken')
 var Users = require('../models/users')
-var _ = require('lodash');
-var merge = require('lodash.merge');
 
 // Signing
 router.post('/signin', function (req, res) {
   Users
     .findOne({ email: req.body.email.toLowerCase().trim() }, (err, result) => {
       if (result) {
-
         Auth.hash_compare(req.body.pass, result.digest, match => {
           if (match) {
-            const jwt_token = JWT.sign({
-                          user_id: result.id,
-                          email: result.email,
-                          user_name: result.name
-                        }, Key.tokenKey)
+            const jwtToken = JWT.sign({
+              user_id: result.id,
+              email: result.email,
+              user_name: result.name
+            }, Key.tokenKey)
 
-            let json = Object.assign({}, result._doc, { token: jwt_token})
+            let json = Object.assign({}, result._doc, { token: jwtToken })
             res.status(200).json(json)
           } else {
             res.status(403).json({ message: 'Invalid Password/Username' })
